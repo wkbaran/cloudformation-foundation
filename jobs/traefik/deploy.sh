35862138e0538b466c955f8696ca29991e35a3d5
Bill Baran <bill.baran@gmail.com>
2025-04-05 20:07:55 -0600

    More traefik

--- jobs/traefik/deploy.sh
+++ jobs/traefik/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 . ./.env
-ssh -i ~/.ssh/minecraftkey.pem ec2-user@jobs.${DOMAIN} mkdir -p traefik
-scp -i ~/.ssh/minecraftkey.pem traefik.toml ec2-user@jobs.${DOMAIN}:traefik/
+ssh -i ~/.ssh/minecraftkey.pem ec2-user@jobs.${DOMAIN} mkdir -p traefik/config
+scp -i ~/.ssh/minecraftkey.pem traefik.yaml ec2-user@jobs.${DOMAIN}:traefik/
+scp -i ~/.ssh/minecraftkey.pem config/dashboard.yaml ec2-user@jobs.${DOMAIN}:traefik/config
 ../jobsdock.sh compose up -d
