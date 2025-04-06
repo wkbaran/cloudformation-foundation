aeb2d85741c88552903b90cc69b338a7267b3f7a
Bill Baran <bill.baran@gmail.com>
2025-04-05 22:48:29 -0600

    Wow, that was a process.  jobs is now traefik dash. frps is blocked except for webhooks.

--- jobs/traefik/deploy.sh
+++ jobs/traefik/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 . ./.env
 ssh -i ~/.ssh/minecraftkey.pem ec2-user@jobs.${DOMAIN} mkdir -p traefik/config
-scp -i ~/.ssh/minecraftkey.pem traefik.yaml ec2-user@jobs.${DOMAIN}:traefik/
-scp -i ~/.ssh/minecraftkey.pem config/dashboard.yaml ec2-user@jobs.${DOMAIN}:traefik/config
+envsubst < traefik.yaml > traefik.gen.yaml
+scp -i ~/.ssh/minecraftkey.pem traefik.gen.yaml ec2-user@jobs.${DOMAIN}:traefik/traefik.yaml
+envsubst < config/dashboard.yaml > config/dashboard.gen.yaml
+scp -i ~/.ssh/minecraftkey.pem config/dashboard.gen.yaml ec2-user@jobs.${DOMAIN}:traefik/config/dashboard.yaml
 ../jobsdock.sh compose up -d
